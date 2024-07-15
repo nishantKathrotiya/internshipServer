5723819eca116ba0bed9021df5d9c8b4870a5e76
Nishant Kathrotiya <113300377+nishantKathrotiya@users.noreply.github.com>
2024-07-15 15:58:53 +0530

    Update Auth.js

--- controller/Auth.js
+++ controller/Auth.js
@@ -124,6 +124,7 @@ const login = async (req, res) => {
         expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
         httpOnly: false,
         sameSite: 'None',
+        secure: true,
       };
       res.cookie("token", token, options);
       return res.status(200).json({
